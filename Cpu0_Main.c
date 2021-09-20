eb6b5cc4ba5c3cb4e324ceada9ebed6e41d6072f
FeiBoNaQi <3170102864@zju.edu.cn>
2021-09-20 16:07:34 +0800

    reformat main to look better

--- Cpu0_Main.c
+++ Cpu0_Main.c
@@ -31,11 +31,10 @@
 #include "IfxScuWdt.h"
 #include "GPIO_RTC.h"
 
-#include "SysSe/Bsp/Bsp.h"   
+#include "SysSe/Bsp/Bsp.h"
 #include "Port/Io/IfxPort_Io.h"
-#include "Multican/Can/IfxMultican_Can.h"       
+#include "Multican/Can/IfxMultican_Can.h"
 #include "Flash_Programming.h"
-
 
 IfxCpu_syncEvent g_cpuSyncEvent = 0;
 
@@ -49,40 +48,40 @@ IfxMultican_Can_MsgObj canSrcMsgObj;
 IfxMultican_Can_MsgObj canRcvMsgObj;
 IfxMultican_Can_MsgObj canRcvMsgObj2;
 
-void encrypt(uint32* message);
-void decrypt(uint32* message);
+void encrypt(uint32 *message);
+void decrypt(uint32 *message);
 void CAN_SendSingle(uint32 id, uint32 high, uint32 low);
 IfxMultican_Status CAN_receiveSingle(IfxMultican_Can_MsgObj *msgObj, IfxMultican_Message *msg);
 
-#define ISR_PRIORITY_CAN_RX         1                           /* Define the CAN RX interrupt priority              */
-#define ISR_PRIORITY_CAN_TX         2                           /* Define the CAN TX interrupt priority              */
-#define ISR_PRIORITY_CAN_RX_2       3                           /* Define the CAN RX interrupt priority              */
+#define ISR_PRIORITY_CAN_RX 1   /* Define the CAN RX interrupt priority              */
+#define ISR_PRIORITY_CAN_TX 2   /* Define the CAN TX interrupt priority              */
+#define ISR_PRIORITY_CAN_RX_2 3 /* Define the CAN RX interrupt priority              */
 
-#define TX_INTERRUPT_SRC_ID         IfxMultican_SrcId_0         /* Transmit interrupt service request ID             */
-#define RX_INTERRUPT_SRC_ID         IfxMultican_SrcId_1         /* Receive interrupt service request ID              */
-#define RX_INTERRUPT_SRC_ID_2       IfxMultican_SrcId_2         /* Receive interrupt service request ID              */
+#define TX_INTERRUPT_SRC_ID IfxMultican_SrcId_0   /* Transmit interrupt service request ID             */
+#define RX_INTERRUPT_SRC_ID IfxMultican_SrcId_1   /* Receive interrupt service request ID              */
+#define RX_INTERRUPT_SRC_ID_2 IfxMultican_SrcId_2 /* Receive interrupt service request ID              */
 IFX_INTERRUPT(canIsrTxHandler, 0, ISR_PRIORITY_CAN_TX);
 IFX_INTERRUPT(canIsrRxHandler, 0, ISR_PRIORITY_CAN_RX);
 IFX_INTERRUPT(canIsrRxHandler_2, 0, ISR_PRIORITY_CAN_RX_2);
 
-#define MEM(address)                *((uint32 *)(address))      /* Macro to simplify the access to a memory address */
-#define DFLASH_STARTING_ADDRESS     0xAF000000                  /* Address of the DFLASH where the data is written  */
+#define MEM(address) *((uint32 *)(address)) /* Macro to simplify the access to a memory address */
+#define DFLASH_STARTING_ADDRESS 0xAF000000  /* Address of the DFLASH where the data is written  */
 
-#define GPIO_IN         &MODULE_P10, 5  /* Port pin for the input  */
-#define GPIO_OUT        &MODULE_P10, 4  /* Port pin for the output  */
+#define GPIO_IN &MODULE_P10, 5  /* Port pin for the input  */
+#define GPIO_OUT &MODULE_P10, 4 /* Port pin for the output  */
 struct UploadMessage uploadMessage = {
-    .lockState = 0  //0 means lock state is unknown
+    .lockState = 0 //0 means lock state is unknown
 };
 uint64 ICCode = 1234567;
 void canIsrRxHandler_2(void)
 {
     IfxPort_togglePin(&MODULE_P33, 9);
     IfxMultican_Status readStatus;
-    IfxMultican_Message rxMsg; 
+    IfxMultican_Message rxMsg;
 
     /* Read the received CAN message and store the status of the operation */
-    readStatus = CAN_receiveSingle(&canRcvMsgObj2, &rxMsg); 
-    
+    readStatus = CAN_receiveSingle(&canRcvMsgObj2, &rxMsg);
+
     if (rxMsg.id == 0 /*ICCode*/)
     {
         ICCode = rxMsg.data[1];
@@ -107,10 +106,10 @@ void canIsrRxHandler_2(void)
         uploadMessage.send = uploadMessage.send | 0x04;
         uploadMessage.lockState = rxMsg.data[0];
     }
-    CAN_SendSingle(0x12345200, rxMsg.data[0], rxMsg.data[1]); 
+    CAN_SendSingle(0x12345200, rxMsg.data[0], rxMsg.data[1]);
 }
 
-#define WAIT_TIME 500   /* Wait time constant in milliseconds   */
+#define WAIT_TIME 500 /* Wait time constant in milliseconds   */
 
 //?????搂脴???????? ???LED1
 void canIsrTxHandler(void)
@@ -122,37 +121,36 @@ void canIsrTxHandler(void)
 void canIsrRxHandler(void)
 {
     IfxPort_togglePin(&MODULE_P33, 9);
-    IfxMultican_Status readStatus;      
-    IfxMultican_Message rxMsg;          
+    IfxMultican_Status readStatus;
+    IfxMultican_Message rxMsg;
     /* Read the received CAN message and store the status of the operation */
-    readStatus = CAN_receiveSingle(&canRcvMsgObj, &rxMsg);     
+    readStatus = CAN_receiveSingle(&canRcvMsgObj, &rxMsg);
 
-    
-    if(rxMsg.id == 0/*ICCode*/)
+    if (rxMsg.id == 0 /*ICCode*/)
     {
         ICCode = rxMsg.data[1];
         ICCode = (ICCode << 32) + rxMsg.data[0];
-    }else if(rxMsg.id == 1/*Can Route 1*/)
+    }
+    else if (rxMsg.id == 1 /*Can Route 1*/)
     {
         uploadMessage.send = uploadMessage.send | 0x01;
         uploadMessage.canRoute1 = rxMsg.data[1];
         uploadMessage.canRoute1 = (uploadMessage.canRoute1 << 32) + rxMsg.data[0];
         uploadMessage.canID1 = 1;
     }
-    else if(rxMsg.id == 2/*Can Route 2*/)
+    else if (rxMsg.id == 2 /*Can Route 2*/)
     {
         uploadMessage.send = uploadMessage.send | 0x02;
         uploadMessage.canRoute2 = rxMsg.data[1];
         uploadMessage.canRoute2 = (uploadMessage.canRoute1 << 32) + rxMsg.data[0];
         uploadMessage.canID2 = 2;
     }
-    else if(rxMsg.id == 3/*Lock state*/)
+    else if (rxMsg.id == 3 /*Lock state*/)
     {
         uploadMessage.send = uploadMessage.send | 0x04;
         uploadMessage.lockState = rxMsg.data[0];
     }
     CAN_SendSingle(0x12345201, rxMsg.data[0], rxMsg.data[1]);
-
 }
 
 void CanApp_init(void)
@@ -162,8 +160,8 @@ void CanApp_init(void)
     IfxMultican_Can_initModuleConfig(&canConfig, &MODULE_CAN);
     // initialize interrupt priority
     canConfig.nodePointer[TX_INTERRUPT_SRC_ID].priority = ISR_PRIORITY_CAN_TX;
-    canConfig.nodePointer[RX_INTERRUPT_SRC_ID].priority = ISR_PRIORITY_CAN_RX;  
-    canConfig.nodePointer[RX_INTERRUPT_SRC_ID_2].priority = ISR_PRIORITY_CAN_RX_2;   
+    canConfig.nodePointer[RX_INTERRUPT_SRC_ID].priority = ISR_PRIORITY_CAN_RX;
+    canConfig.nodePointer[RX_INTERRUPT_SRC_ID_2].priority = ISR_PRIORITY_CAN_RX_2;
     // initialize module
     IfxMultican_Can_initModule(&can, &canConfig);
 
@@ -184,46 +182,45 @@ void CanApp_init(void)
     canMsgObjConfig.frame = IfxMultican_Frame_transmit;
     canMsgObjConfig.control.messageLen = IfxMultican_DataLengthCode_8;
     canMsgObjConfig.control.extendedFrame = TRUE;
-    canMsgObjConfig.txInterrupt.enabled = TRUE;  
-    canMsgObjConfig.txInterrupt.srcId = TX_INTERRUPT_SRC_ID;  
+    canMsgObjConfig.txInterrupt.enabled = TRUE;
+    canMsgObjConfig.txInterrupt.srcId = TX_INTERRUPT_SRC_ID;
     // initialize receive message object
     IfxMultican_Can_MsgObj_init(&canSrcMsgObj, &canMsgObjConfig);
 
     IfxMultican_Can_MsgObj_initConfig(&canMsgObjConfig, &canSrcNode);
     canMsgObjConfig.msgObjId = 1;
-    canMsgObjConfig.messageId = 0x00000000;     
+    canMsgObjConfig.messageId = 0x00000000;
     canMsgObjConfig.acceptanceMask = 0xFFFFFFF0;
     //canMsgObjConfig.acceptanceMask = 0x7FFFFFFFUL;
     canMsgObjConfig.frame = IfxMultican_Frame_receive;
     canMsgObjConfig.control.messageLen = IfxMultican_DataLengthCode_8;
     canMsgObjConfig.control.extendedFrame = TRUE;
-    canMsgObjConfig.rxInterrupt.enabled = TRUE;   
-    canMsgObjConfig.rxInterrupt.srcId = RX_INTERRUPT_SRC_ID;   
+    canMsgObjConfig.rxInterrupt.enabled = TRUE;
+    canMsgObjConfig.rxInterrupt.srcId = RX_INTERRUPT_SRC_ID;
     // initialize message object
     IfxMultican_Can_MsgObj_init(&canRcvMsgObj, &canMsgObjConfig);
 
-
     IfxMultican_Can_MsgObj_initConfig(&canMsgObjConfig, &canSrcNode);
     canMsgObjConfig.msgObjId = 2;
-    canMsgObjConfig.messageId = 0x12345678;   
+    canMsgObjConfig.messageId = 0x12345678;
     canMsgObjConfig.acceptanceMask = 0xFFFFFFFF;
-    //canMsgObjConfig.acceptanceMask = 0x7FFFFFFFUL; 
+    //canMsgObjConfig.acceptanceMask = 0x7FFFFFFFUL;
     canMsgObjConfig.frame = IfxMultican_Frame_receive;
     canMsgObjConfig.control.messageLen = IfxMultican_DataLengthCode_8;
     canMsgObjConfig.control.extendedFrame = TRUE;
-    canMsgObjConfig.rxInterrupt.enabled = TRUE;    
-    canMsgObjConfig.rxInterrupt.srcId = RX_INTERRUPT_SRC_ID_2;    
+    canMsgObjConfig.rxInterrupt.enabled = TRUE;
+    canMsgObjConfig.rxInterrupt.srcId = RX_INTERRUPT_SRC_ID_2;
     // initialize message object
     IfxMultican_Can_MsgObj_init(&canRcvMsgObj2, &canMsgObjConfig);
 }
 
-void encrypt(uint32* message)
+void encrypt(uint32 *message)
 {
-    *message = *message +1;
+    *message = *message + 1;
 }
-void decrypt(uint32* message)
+void decrypt(uint32 *message)
 {
-    *message = *message -1;
+    *message = *message - 1;
 }
 void CAN_SendSingle(uint32 id, uint32 high, uint32 low)
 {
@@ -234,8 +231,8 @@ void CAN_SendSingle(uint32 id, uint32 high, uint32 low)
     IfxMultican_Message_init(&txMsg, id, low, high, IfxMultican_DataLengthCode_8);
 
     // Transmit Data
-    while( IfxMultican_Can_MsgObj_sendMessage(&canSrcMsgObj, &txMsg) == IfxMultican_Status_notSentBusy );
-
+    while (IfxMultican_Can_MsgObj_sendMessage(&canSrcMsgObj, &txMsg) == IfxMultican_Status_notSentBusy)
+        ;
 }
 
 IfxMultican_Status CAN_receiveSingle(IfxMultican_Can_MsgObj *msgObj, IfxMultican_Message *msg)
@@ -243,7 +240,7 @@ IfxMultican_Status CAN_receiveSingle(IfxMultican_Can_MsgObj *msgObj, IfxMultican
 
     IfxMultican_Status readStatus;
     /* Read the received CAN message and store the status of the operation */
-    readStatus = IfxMultican_Can_MsgObj_readMessage(msgObj, msg); 
+    readStatus = IfxMultican_Can_MsgObj_readMessage(msgObj, msg);
     decrypt(&(msg->data[0]));
     decrypt(&(msg->data[1]));
     return readStatus;
@@ -263,11 +260,11 @@ uint64 convertDateToUnixTime(void)
     uint64 t;
 
     //Year
-    y = clock_data[6]/16*10+clock_data[6]%16+2000;
+    y = clock_data[6] / 16 * 10 + clock_data[6] % 16 + 2000;
     //Month of year
-    m = clock_data[4]/16*10+clock_data[4]%16;
+    m = clock_data[4] / 16 * 10 + clock_data[4] % 16;
     //Day of month
-    d = clock_data[3]/16*10+clock_data[3]%16;
+    d = clock_data[3] / 16 * 10 + clock_data[3] % 16;
 
     //January and February are counted as months 13 and 14 of the previous year
     if (m <= 2)
@@ -285,7 +282,7 @@ uint64 convertDateToUnixTime(void)
     //Convert days to seconds
     t *= 86400;
     //Add hours, minutes and seconds
-    t += (3600 * (clock_data[2]/16*10+clock_data[2]%16)) + (60 * (clock_data[1]/16*10+clock_data[1]%16)) + (clock_data[0]/16*10+clock_data[0]%16);
+    t += (3600 * (clock_data[2] / 16 * 10 + clock_data[2] % 16)) + (60 * (clock_data[1] / 16 * 10 + clock_data[1] % 16)) + (clock_data[0] / 16 * 10 + clock_data[0] % 16);
 
     //Return Unix time
     return t;
@@ -295,7 +292,7 @@ uint64 convertDateToUnixTime(void)
 @currentTime variable address passed in, used to store the current time;
 @return 0 means success, 1 means failure
 */
-int GetCurrentTime(uint64* currentTime)
+int GetCurrentTime(uint64 *currentTime)
 {
     //dummy function that increase the time;
     CE_On();
@@ -305,7 +302,8 @@ int GetCurrentTime(uint64* currentTime)
     return 0;
 }
 
-void initOnsiteLock(void){
+void initOnsiteLock(void)
+{
 
     IfxPort_setPinMode(GPIO_IN, IfxPort_Mode_inputPullDown);
     IfxPort_setPinMode(GPIO_OUT, IfxPort_Mode_outputPushPullGeneral);
@@ -315,25 +313,24 @@ void initOnsiteLock(void){
 int core0_main(void)
 {
     struct AuthInfo authInfo[BufferLen];
-    struct AuthInfo temp[BufferLen]={0};
-    struct BackEndInfo backEndInfo ={
-        .remoteLockControl = 0
-    };
-    uint64 currentTime=0;
-    int LockStatus;     //useless for now
+    struct AuthInfo temp[BufferLen] = {0};
+    struct BackEndInfo backEndInfo = {
+        .remoteLockControl = 0};
+    uint64 currentTime = 0;
+    int LockStatus; //useless for now
     //the two least significant bits determine the desired lock state: 0 means no operation, 1 means open,2 means lock
     //the third bit is on if lock is determined by timestamp
     //the fourth bit is on if lock is determined by cloud command
-    int LockControl=0;    
+    int LockControl = 0;
     int ret;
     IfxCpu_enableInterrupts();
-    
+
     /* !!WATCHDOG0 AND SAFETY WATCHDOG ARE DISABLED HERE!!
      * Enable the watchdogs and service them periodically if it is required
      */
     IfxScuWdt_disableCpuWatchdog(IfxScuWdt_getCpuWatchdogPassword());
     IfxScuWdt_disableSafetyWatchdog(IfxScuWdt_getSafetyWatchdogPassword());
-    
+
     /* Wait for CPU sync event */
     IfxCpu_emitEvent(&g_cpuSyncEvent);
     IfxCpu_waitEvent(&g_cpuSyncEvent, 1);
@@ -345,7 +342,6 @@ int core0_main(void)
     IfxPort_setPinMode(&MODULE_P33, 9, IfxPort_Mode_outputPushPullGeneral);
     IfxPort_setPinHigh(&MODULE_P33, 8);
     IfxPort_setPinLow(&MODULE_P33, 9);
-
 
     CanApp_init();
 
@@ -366,78 +362,82 @@ int core0_main(void)
     readAuthInfo(authInfo);
     for (int i = 0; i < BufferLen; i++)
     {
-        backEndInfo.authInfo[i]=authInfo[i];
+        backEndInfo.authInfo[i] = authInfo[i];
     }
-    init_GPIOs(); 
+    init_GPIOs();
     initOnsiteLock();
-    while(1)
-    {   
-        
+    while (1)
+    {
+
         //restore lock control to no operation
-        LockControl=0;
-//        CAN_SendSingle(0x01234567,0x5678,0x1234);   //碌脥脦禄 赂脽脦禄
-// //
-//        waitTime(IfxStm_getTicksFromMilliseconds(BSP_DEFAULT_TIMER, WAIT_TIME));    /* Wait 500 milliseconds            */
-//        IfxPort_togglePin(&MODULE_P33, 8);
-    /*1. get the current time*/
-    ret = GetCurrentTime(&currentTime);
-    if(ret)
-    {
-        while(1);
-    }
-    /*2. authenticate if the driver can use the car to determine the desiring lock state*/
-    if (backEndInfo.remoteLockControl == 0)
-    {
-        for (int i = 0; i < BufferLen; i++)
+        LockControl = 0;
+        //        CAN_SendSingle(0x01234567,0x5678,0x1234);   //碌脥脦禄 赂脽脦禄
+        // //
+        //        waitTime(IfxStm_getTicksFromMilliseconds(BSP_DEFAULT_TIMER, WAIT_TIME));    /* Wait 500 milliseconds            */
+        //        IfxPort_togglePin(&MODULE_P33, 8);
+        /*1. get the current time*/
+        ret = GetCurrentTime(&currentTime);
+        if (ret)
         {
-            if (authInfo[i].indentification == ICCode && currentTime > authInfo[i].startTime && currentTime < authInfo[i].endTime)
+            while (1)
+                ;
+        }
+        /*2. authenticate if the driver can use the car to determine the desiring lock state*/
+        if (backEndInfo.remoteLockControl == 0)
+        {
+            for (int i = 0; i < BufferLen; i++)
             {
-                //1 means open, the third bit is on means the lock state is determined by time stamp
-                LockControl = 1 | 4;
+                if (authInfo[i].indentification == ICCode && currentTime > authInfo[i].startTime && currentTime < authInfo[i].endTime)
+                {
+                    //1 means open, the third bit is on means the lock state is determined by time stamp
+                    LockControl = 1 | 4;
+                    break;
+                }
+            }
+        }
+        else if (backEndInfo.remoteLockControl == 1)
+        {
+            //1 means open, the forth bit is on means the lock state is determined by cloud command
+            LockControl = 1 | 8;
+        }
+        else if (backEndInfo.remoteLockControl == 2)
+        {
+            //2 means closed, the forth bit is on means the lock state is determined by cloud command
+            LockControl = 2 | 8;
+        }
+
+        // on site lock control is of higher precedence, so it is placed later
+        if ((IfxPort_getPinState(GPIO_IN)) == 0x01) //high voltage means lock the car , low voltage means no action
+        {
+            //2 means closed, the fifth bit is on means the lock state is determined by on site physical device
+            LockControl = 2 | 16;
+        }
+        /*3. send can message to the electric control module that indicate whether the lock should be open or on*/
+        CAN_SendSingle(0x98354990, LockControl, 0);
+        /*4. read usart port for message from the 4G module, fill in the struct backEndInfo zeng*/
+        /*5. send the struct UploadMessage to the cloud according to the send value zeng*/
+        /*6. compare AuthInfo and BackEndInfo, update AuthInfo and dflash if they are not equal*/
+        int i = 0;
+        for (; i < BufferLen; i++)
+        {
+            if (authInfo[i].indentification != backEndInfo.authInfo[i].indentification ||
+                authInfo[i].startTime != backEndInfo.authInfo[i].startTime ||
+                authInfo[i].endTime != backEndInfo.authInfo[i].endTime)
+            {
                 break;
             }
         }
-    }else if(backEndInfo.remoteLockControl == 1)
-    {
-        //1 means open, the forth bit is on means the lock state is determined by cloud command
-        LockControl = 1 | 8;
-    }else if(backEndInfo.remoteLockControl == 2)
-    {
-        //2 means closed, the forth bit is on means the lock state is determined by cloud command
-        LockControl = 2 | 8;
-    }
-
-    // on site lock control is of higher precedence, so it is placed later
-    if ((IfxPort_getPinState(GPIO_IN)) == 0x01)     //high voltage means lock the car , low voltage means no action
-    {
-        //2 means closed, the fifth bit is on means the lock state is determined by on site physical device
-        LockControl = 2 | 16;
-    }
-    /*3. send can message to the electric control module that indicate whether the lock should be open or on*/ 
-    CAN_SendSingle(0x98354990, LockControl, 0);
-    /*4. read usart port for message from the 4G module, fill in the struct backEndInfo zeng*/
-    /*5. send the struct UploadMessage to the cloud according to the send value zeng*/
-    /*6. compare AuthInfo and BackEndInfo, update AuthInfo and dflash if they are not equal*/
-    int i = 0;
-    for (; i < BufferLen; i++)
-    {
-        if (authInfo[i].indentification != backEndInfo.authInfo[i].indentification ||
-            authInfo[i].startTime != backEndInfo.authInfo[i].startTime ||
-            authInfo[i].endTime != backEndInfo.authInfo[i].endTime)
+        if (i != BufferLen)
         {
-            break;
+            for (i = 0; i < BufferLen; i++)
+            {
+                authInfo[i].indentification = backEndInfo.authInfo[i].indentification;
+                authInfo[i].startTime = backEndInfo.authInfo[i].startTime;
+                authInfo[i].endTime = backEndInfo.authInfo[i].endTime;
+            }
+            eraseDflash();
+            writeAuthInfo(authInfo);
         }
-    }
-    if( i != BufferLen){
-        for (i=0; i < BufferLen; i++)
-        {
-            authInfo[i].indentification = backEndInfo.authInfo[i].indentification;
-            authInfo[i].startTime = backEndInfo.authInfo[i].startTime;
-            authInfo[i].endTime = backEndInfo.authInfo[i].endTime;
-        }
-        eraseDflash();
-        writeAuthInfo(authInfo);
-    }
     }
     return (1);
 }
